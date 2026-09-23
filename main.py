a855ef994a8f09cffd97f54e78bf7eeebc4ee21e
qwertyuiop161 <84034410+qwertyuiop161@users.noreply.github.com>
2026-09-23 15:13:27 +0000

    face reveal pt 3!!!!!!

--- main.py
+++ main.py
@@ -2,54 +2,40 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.util import random_noise
+import os
 
-image = cv2.imread('image.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+folder = 'images'
+os.makedirs('speckle', exist_ok=True)
 
-image_float = image / 255.0
-print("customize params? y for yes anything for no")
-x = input("")
-se=0.01
-g=0.01
-sp=0.02
-if (x=="y"):
-  se = float(input("0-1 speckle"))
-  g = float(input("0-1 gaussian"))
-  sp = float(input("0-1 salt&pepper"))
-speckle = random_noise(image_float, mode='speckle', var=se)
-gaussian = random_noise(image_float, mode='gaussian', var=g)
-sp = random_noise(image_float, mode='s&p', amount=sp)
-noisy_uintsp = np.clip(sp * 255, 0, 255).astype(np.uint8)
-noisy_uintg = np.clip(gaussian * 255, 0, 255).astype(np.uint8)
-noisy_uintse = np.clip(speckle * 255, 0, 255).astype(np.uint8)
+file_names = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+print("type 1 to customize params")
+x = int(input(""))
+se = 0.01
+if (x==1):
+    se=float(input("enter speckle intensity 0-1: "))
 
-noisy_sp = cv2.cvtColor(noisy_uintsp, cv2.COLOR_RGB2BGR)
-noisy_se = cv2.cvtColor(noisy_uintse, cv2.COLOR_RGB2BGR)
-noisy_g = cv2.cvtColor(noisy_uintg, cv2.COLOR_RGB2BGR)
-
-cv2.imwrite('saltandpepper.jpg', noisy_sp)
-cv2.imwrite('speckle.jpg', noisy_se)
-cv2.imwrite('guassian.jpg', noisy_g)
-
-fig, axes = plt.subplots(3, 3, figsize=(13, 12))
-
-axes[0, 0].imshow(image_float)
-axes[0, 0].set_title('Original')
-axes[0, 1].imshow(speckle)
-axes[0, 1].set_title('Speckle (var=' + str(se) + ')')
-
-axes[1, 0].imshow(image_float)
-axes[1, 0].set_title('Original')
-axes[1, 1].imshow(gaussian)
-axes[1, 1].set_title('Gaussian (var=' + str(g) + ')')
-
-axes[2, 0].imshow(image_float)
-axes[2, 0].set_title('Original')
-axes[2, 1].imshow(sp)
-axes[2, 1].set_title('S&P (amount=' + str(sp) + ')')
-
-for ax in axes.ravel():
-  ax.axis('off')
-
-plt.tight_layout()
-plt.show()
+for item in file_names:
+    image = cv2.imread(os.path.join(folder, item))
+    
+        
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_float = image / 255.0
+    
+    speckle = random_noise(image_float, mode='speckle', var=se)
+    noisy_uintse = np.clip(speckle * 255, 0, 255).astype(np.uint8)
+    noisy_se = cv2.cvtColor(noisy_uintse, cv2.COLOR_RGB2BGR)
+    
+    cv2.imwrite('speckle/' + os.path.splitext(item)[0] + '.jpg', noisy_se)
+    
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    
+    axes[0].imshow(image_float)
+    axes[0].set_title('Original')
+    axes[0].axis('off')
+    
+    axes[1].imshow(speckle)
+    axes[1].set_title(f'Speckle (var={se})')
+    axes[1].axis('off')
+    
+    plt.tight_layout()
+    plt.show()
